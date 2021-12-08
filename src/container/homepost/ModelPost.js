7626e73d637ad646d48cc1425da36432e4ead68a
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2021-12-08 21:49:10 +0500

    hamza

---
+++ src/container/homepost/ModelPost.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const ModelPost=()=>{
+  const [name,setName]=   useState("")
+  const [email,setEmail]=useState("")
+  const   
+    return(
+       <div>
+       
+       </div>
+    )
+}
+export default ModelPost;
--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,9 +1,10 @@
 import MenuAppBar from "../../container/AppBar"
+import BasicModal from "../../container/modal/Modal";
 const Home=()=>{
     return(
         <div>
     <MenuAppBar title="Home"/>    
-
+       <BasicModal/>
            
         </div>
     )
